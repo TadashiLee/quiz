38a58bbf34b332cc4b3673a29130621fbe57b2ae
TadashiLee <584638605@qq.com>
2020-10-23 10:19:50 +0800

    [wenchang.li]feat:implemnt get products

--- back-end/src/main/java/service/ProductService.java
+++ back-end/src/main/java/service/ProductService.java
@@ -1,4 +1,24 @@
 package service;
 
+import dto.Product;
+import org.springframework.stereotype.Service;
+import repository.ProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        return productRepository.findAll().stream()
+                .map(Product::fromEntityToProduct)
+                .collect(Collectors.toList());
+    }
 }
--- back-end/src/test/java/controller/ProductControllerTest.java
+++ back-end/src/test/java/controller/ProductControllerTest.java
@@ -1,4 +1,61 @@
 package controller;
 
+import entity.ProductEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import repository.ProductRepository;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ProductControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    void should_get_all_products() throws Exception {
+        productRepository.save(ProductEntity.builder()
+                .name("可乐")
+                .price(1.00)
+                .unit("瓶")
+                .image("colar")
+                .build());
+        productRepository.save(ProductEntity.builder()
+                .name("雪碧")
+                .price(2.50)
+                .unit("听")
+                .image("spring")
+                .build());
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is("可乐")))
+                .andExpect(jsonPath("$[0].price", is(1.00)))
+                .andExpect(jsonPath("$[0].unit", is("瓶")))
+                .andExpect(jsonPath("$[0].image", is("colar")))
+                .andExpect(jsonPath("$[1].name", is("雪碧")))
+                .andExpect(jsonPath("$[1].price", is(2.50)))
+                .andExpect(jsonPath("$[1].unit", is("听")))
+                .andExpect(jsonPath("$[1].image", is("spring")));
+    }
 }
