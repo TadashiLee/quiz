f1cccca4b6907fd2dcef85c2f66fb6142f8dd904
TadashiLee <584638605@qq.com>
2020-10-23 09:16:02 +0800

    [wenchang.li]feat:implemnt repository

--- back-end/src/main/java/repository/OrderRepository.java
+++ back-end/src/main/java/repository/OrderRepository.java
@@ -1,4 +1,15 @@
 package repository;
 
-public interface OrderRepository {
+import entity.OrderEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends CrudRepository<OrderEntity, Integer> {
+
+    List<OrderEntity> findAll();
+    Optional<OrderEntity> findByProductId(Integer productId);
 }
--- back-end/src/main/java/repository/ProductRepository.java
+++ back-end/src/main/java/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package repository;
 
-public interface ProductRepository {
+import entity.ProductEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductEntity, Integer> {
+
+    List<ProductEntity> findAll();
+    boolean existsByName(String name);
 }
